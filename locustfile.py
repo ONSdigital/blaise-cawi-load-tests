3f069c3296e1c1ca6a463d8d0e46d20f5ef9ad7d
Sam Bryant <sam.bryant@armakuni.com>
2021-08-11 09:32:03 +0100

    Swap back to realistic user behaviour in load tests

--- locustfile.py
+++ locustfile.py
@@ -21,47 +21,47 @@ class CAWI(FastHttpUser):
         print(uac["postcode"])
         print(uac["caseid"])
         self.client.get("/auth/login")
-        # time.sleep(3)
+        time.sleep(3)
         self.client.post("/auth/login", {"uac": uac["uaccode"]})
         self.client.get("/auth/login/postcode")
-        # self.client.post("/auth/login/postcode", {"postcode": uac["postcode"]})
-        # time.sleep(1)
-        # self.client.get(f"/{instrument_name}/")
-        # self.client.post(
-        #     f"/{instrument_name}/api/application/start_interview",
-        #     json={
-        #         "RuntimeParameters": {
-        #             "ServerPark": "gusty",
-        #             "InstrumentId": f"{instrument_id}",
-        #             "MeasurePageTimes": False,
-        #             "IsPreview": False,
-        #             "IsTesting": False,
-        #             "KeyValue": uac["caseid"],
-        #             "LayoutSet": "CAWI-Web_Large",
-        #             "Mode": "CAWI",
-        #         },
-        #         "ClientFeatures": {
-        #             "Browser": 3,
-        #             "Device": 2,
-        #             "Height": 790,
-        #             "Language": "en-GB",
-        #             "Latitude": 0,
-        #             "Longitude": 0,
-        #             "OS": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-        #             "PixelRatio": 1,
-        #             "Platform": 1,
-        #             "RecorderAvailable": False,
-        #             "Referrer": "",
-        #             "ReferrerUrl": f"https://{self.host}/auth/login/postcode",
-        #             "ScreenHeight": 790,
-        #             "ScreenWidth": 1720,
-        #             "ScrollbarSize": 15,
-        #             "TouchEnabled": False,
-        #             "Width": 1720,
-        #         },
-        #         "AccessToken": None,
-        #         "RefreshToken": None,
-        #     },
-        # )
-        # time.sleep(5)
+        time.sleep(2)
+        self.client.post("/auth/login/postcode", {"postcode": uac["postcode"]})
+        self.client.get(f"/{instrument_name}/")
+        self.client.post(
+            f"/{instrument_name}/api/application/start_interview",
+            json={
+                "RuntimeParameters": {
+                    "ServerPark": "gusty",
+                    "InstrumentId": f"{instrument_id}",
+                    "MeasurePageTimes": False,
+                    "IsPreview": False,
+                    "IsTesting": False,
+                    "KeyValue": uac["caseid"],
+                    "LayoutSet": "CAWI-Web_Large",
+                    "Mode": "CAWI",
+                },
+                "ClientFeatures": {
+                    "Browser": 3,
+                    "Device": 2,
+                    "Height": 790,
+                    "Language": "en-GB",
+                    "Latitude": 0,
+                    "Longitude": 0,
+                    "OS": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+                    "PixelRatio": 1,
+                    "Platform": 1,
+                    "RecorderAvailable": False,
+                    "Referrer": "",
+                    "ReferrerUrl": f"https://{self.host}/auth/login/postcode",
+                    "ScreenHeight": 790,
+                    "ScreenWidth": 1720,
+                    "ScrollbarSize": 15,
+                    "TouchEnabled": False,
+                    "Width": 1720,
+                },
+                "AccessToken": None,
+                "RefreshToken": None,
+            },
+        )
+        time.sleep(5)
         self.client.get("/auth/logout")
