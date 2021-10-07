525dccbf8f0dd6f205afc91463fddce740c92e9b
Sam Bryant <srby@hotmail.com>
2021-10-07 11:37:44 +0100

    Add the seed iteration to the secret names

--- k8s-locust.sh
+++ k8s-locust.sh
@@ -7,9 +7,11 @@ gcloud container clusters get-credentials locust-gke-cluster --region=europe-wes
 kubectl create configmap my-loadtest-locustfile --from-file locustfile.py ||
   kubectl create configmap my-loadtest-locustfile --from-file locustfile.py -o yaml --dry-run | kubectl replace -f -
 
+SEED_ITER=0
 for seed_file in seed-data*.csv; do
-  kubectl create secret generic seed-data --from-file "${seed_file}" ||
-    kubectl create secret generic seed-data --from-file "${seed_file}" -o yaml --dry-run | kubectl replace -f -
+  kubectl create secret generic "seed-data${SEED_ITER}" --from-file "${seed_file}" ||
+    kubectl create secret generic "seed-data${SEED_ITER}" --from-file "${seed_file}" -o yaml --dry-run | kubectl replace -f -
+  SEED_ITER=$((SEED_ITER + 1))
 done
 
 helm repo add deliveryhero https://charts.deliveryhero.io/ || true
